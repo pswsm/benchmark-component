76d2bb5f06f434d623829c2cf2d85b623cc7df39
pswsm <pauseawesome@protonmail.com>
2023-02-22 15:56:29 +0100

    Add dark to class to svelte.body parent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,11 @@
 		title: 'Example Benchmark',
 		apiUrl: '',
 		pageDescription: 'A description for our benchmark example.',
-		navObjs: [{text: 'some nav elem', link: '#'}, {text: 'another nav elem', link: '#'}]
+		navObjs: [{text: 'some nav elem', link: '#'}, {text: 'another nav elem', link: '#'}],
+		footerProps: {images: [{src: "https://club.aina.bsc.es/images/generalitat-red-white.png", alt: "Generalitat de Catalunya", href: "https://gencat.cat"}, {src: "https://club.aina.bsc.es/images/BSC-white-small.png", alt: "Barcelona Supercomputing Center", href: "https://bsc.es"}]}
 	}
 
 </script>
-<Benchmark {...benchmarkData} />
+<div class="dark">
+	<Benchmark {...benchmarkData} />
+</div>
