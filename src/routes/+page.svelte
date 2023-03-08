d9d28fee297a54fa3cd951ac94558ccd6a3d3b84
pswsm <pauseawesome@protonmail.com>
2023-03-08 11:26:25 +0100

    Prop renames, cleaner code"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import Benchmark from "$lib/BenchmarkComponent/Benchmark.svelte";
 	let benchmarkData = {
-		title: 'Example Benchmark',
-		pageDescription: 'A description for our benchmark example.',
+		pageTitle: 'Benchmark',
+		pageDescription: 'A description for our benchmark.',
 		navObjs: [{text: 'some nav elem', link: '#'}, {text: 'another nav elem', link: '#'}],
-		footerProps: {images: [
-									{src: "https://club.aina.bsc.es/images/generalitat-red-white.png", alt: "Generalitat de Catalunya", href: "https://gencat.cat"},
-									{src: "https://club.aina.bsc.es/images/BSC-white-small.png", alt: "Barcelona Supercomputing Center", href: "https://bsc.es"}
-									]},
-		}
+		footerProps: {
+			images: [
+				{src: "https://club.aina.bsc.es/images/generalitat-red-white.png", alt: "Generalitat de Catalunya", href: "https://gencat.cat"},
+				{src: "https://club.aina.bsc.es/images/BSC-white-small.png", alt: "Barcelona Supercomputing Center", href: "https://bsc.es"}
+			]
+		},
+	}
+let apiUrls = {
+	headerEndpoint: "https://run.mocky.io/v3/11a5dd03-65aa-42ca-9268-6e1fdf23d781",
+	dataEndpoint: "https://run.mocky.io/v3/83910796-181a-45ed-a008-1a99ec3544d5"
+}
 </script>
 <div class="dark">
-	<Benchmark {...benchmarkData} apiUrls={{headerEndpoint: "https://run.mocky.io/v3/11a5dd03-65aa-42ca-9268-6e1fdf23d781", dataEndpoint: "https://run.mocky.io/v3/83910796-181a-45ed-a008-1a99ec3544d5"}}  />
+	<Benchmark {...benchmarkData} apiUrls={apiUrls}  />
 </div>
