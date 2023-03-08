7055671a045229cddce6cee7800cbd14dfbc7b40
Pau Figueras <pau@pswsm.cat>
2023-03-08 13:20:20 +0100

    Add default min-height, gradient for image and centered text for text

--- src/lib/BenchmarkComponent/Banner.svelte
+++ src/lib/BenchmarkComponent/Banner.svelte
@@ -4,11 +4,10 @@
 	export let imgSrc: string;
 	export let bgcolor: string;
 	export let text: string | null = null;
+	export let minHeight: string = "300px"
 </script>
 {#if text}
-	<div style="background-color: { bgcolor };" class="text-center"><p class="h2">{ text }</p></div>
+	<div class="min-h-[{minHeight}] text-center flex justify-center items-center" style:background-color='{bgcolor}'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
 {:else}
-	<div style="background-image: linear-gradient(to botttom, rgba(1, 1, 1, 0) 50%, { bgcolor }), url({ imgSrc }); background-repeat: no-repeat, no-repeat; background-size: auto, cover;">
-		<div class="container"></div>
-	</div>
+	<div class="min-h-[{minHeight}]" style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0), {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
 {/if}
