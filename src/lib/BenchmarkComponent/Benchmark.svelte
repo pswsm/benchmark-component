c15fa06d13477003346dd6b37310fc547c8fbbb8
Pau Figueras <pau@pswsm.cat>
2023-03-08 13:22:07 +0100

    Fix tableContent type

--- src/lib/BenchmarkComponent/Benchmark.svelte
+++ src/lib/BenchmarkComponent/Benchmark.svelte
@@ -15,7 +15,7 @@ export let colorScheme: { textColor?: "black" | "white", color: "red" | "white" 
 // let modelsData: Promise<any> = fetch(apiUrl);
 // let tableHeaders: Promise<{tasks: object[]}>;
 let tableHeaders: Promise<{ tasks: { _id: { "$oid": string }[], description: string, name: string }[] }> = fetch(apiUrls.headerEndpoint).then( (res) => res.json() ).catch( (err) => err );
-let tableContent: Promise<{model: string, score: number, submitted_by: string, URL: string, task1: number, task2: number, task3: number, task4: number}[]> = fetch(apiUrls.dataEndpoint).then((res) => res.json());
+let tableContent: Promise<{ results: {model: string, score: number, submitted_by: string, URL: string, task1: number, task2: number, task3: number, task4: number}[] }> = fetch(apiUrls.dataEndpoint).then((res) => res.json());
 
 
 </script>
