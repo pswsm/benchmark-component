174361196ae5746b42f5bff15935a5b952c9cd13
pswsm <pauseawesome@protonmail.com>
2023-03-08 11:22:55 +0100

    Create banner component

---
+++ src/lib/BenchmarkComponent/Banner.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Img } from "flowbite-svelte";
+
+	export let imgSrc: string;
+	export let bgcolor: string;
+	export let text: string | null;
+</script>
+{#if text}
+	<div style="background-color: { bgcolor };" class="text-center"><p class="h2">{ text }</p></div>
+{:else}
+	<div style="background-image: linear-gradient(to botttom, rgba(1, 1, 1, 0) 50%, { bgcolor }), url({ imgSrc }); background-repeat: no-repeat, no-repeat; background-size: auto, cover;">
+		<div class="container"></div>
+	</div>
+{/if}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 // Reexport your entry components here
-export { default as Benchmark } from "./BenchmarkComponent/Benchmark.svelte"
+export { default as Benchmark } from "./BenchmarkComponent/Benchmark.svelte";
+export { default as Banner } from "./BenchmarkComponent/Banner.svelte";
