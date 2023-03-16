cbd1674480585e0b00b70bf009cdd52ba6964358
Pau Figueras <pau@pswsm.cat>
2023-03-16 21:10:15 +0100

    Functional datatable (finally yay!)

---
+++ src/lib/BenchmarkComponent/BenchmarkTable2.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+import { Spinner } from 'flowbite-svelte';
+import { onMount } from "svelte";
+import { DataHandler, Datatable, Th } from "@vincjo/datatables";
+import type { Readable } from 'svelte/store';
+
+export let headerUrl: string;
+export let contentUrl: string;
+export let spinnerColor: "red" | "white" | "gray" | "white" | "blue" = 'red';
+
+let ptData: {model: string, score: number, submitted_by: string, url: string, task1: number, task2: number, task3: number, task4: number}[];
+let rawHeaders: {_id: {$oid: string}, name: string, description: string}[];
+const staticHeaders: string[] = ['rank', 'model', 'score', 'submitted_by', 'URL']
+
+let handler: DataHandler;
+let rows: Readable<any[]>;
+
+onMount( async () => {
+	let table: Response = await fetch(contentUrl);
+	let headers: Response = await fetch(headerUrl);
+	ptData = await table.json().then(data => data.results);
+	rawHeaders = await headers.json().then(tasks => tasks.tasks);
+	handler = new DataHandler(ptData, { rowsPerPage: 10 });
+	rows = handler.getRows();
+})
+</script>
+{#if handler}
+<Datatable {handler}>
+	<table>
+		<thead>
+			<tr>
+			{#if rawHeaders}
+				{#each staticHeaders as sHeader}
+					<Th {handler}>{sHeader}</Th>
+				{/each}
+				{#each rawHeaders as header}
+					<Th {handler}>{header.name}</Th>
+				{/each}
+			{:else}
+				<Spinner size={"8"} color={spinnerColor}/>
+			{/if}
+			</tr>
+		</thead>
+		<tbody>
+		{#each $rows as row, idx}
+			<tr>
+				<td>{idx + 1}</td>
+				{#each Object.keys(row) as rowKey}
+					<td>{row[rowKey]}</td>
+				{/each}
+			</tr>
+		{:else}
+			<Spinner size={"8"} color={spinnerColor}/>
+		{/each}
+		</tbody>
+	</table>
+</Datatable>
+{:else}
+	<div class="text-center">
+		<Spinner size={"8"} color={spinnerColor}/>
+	</div>
+{/if}
