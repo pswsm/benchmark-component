6200050bf046e6c3782d429a4f2c99207adbd417
Pau Figueras <pau@pswsm.cat>
2023-03-16 21:09:37 +0100

    Add new exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,6 @@
 // Reexport your entry components here
 export { default as Benchmark } from "./BenchmarkComponent/Benchmark.svelte";
 export { default as Banner } from "./BenchmarkComponent/Banner.svelte";
+export { default as Navbar } from "./BenchmarkComponent/Navbar.svelte";
+export { default as Table } from "./BenchmarkComponent/BenchmarkTable.svelte";
+export { default as Table2 } from "./BenchmarkComponent/BenchmarkTable2.svelte";
