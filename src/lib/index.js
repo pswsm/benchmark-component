18bedbcadbf5a8fc963c851b18608deab5f4df26
Pau Figueras <pau@pswsm.cat>
2023-03-20 12:53:08 +0100

    Create contact component

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ export { default as Benchmark } from "./BenchmarkComponent/Benchmark.svelte";
 export { default as Banner } from "./BenchmarkComponent/Banner.svelte";
 export { default as Navbar } from "./BenchmarkComponent/Navbar.svelte";
 export { default as Table } from "./BenchmarkComponent/BenchmarkTable.svelte";
+export { default as Contact } from "./BenchmarkComponent/Contact.svelte";
