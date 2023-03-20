d62e9334124b8c1d4d1b56026a46ed52233e47ae
Pau Figueras <pau@pswsm.cat>
2023-03-20 12:51:32 +0100

    Change Table2 to Table

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,4 +3,3 @@ export { default as Benchmark } from "./BenchmarkComponent/Benchmark.svelte";
 export { default as Banner } from "./BenchmarkComponent/Banner.svelte";
 export { default as Navbar } from "./BenchmarkComponent/Navbar.svelte";
 export { default as Table } from "./BenchmarkComponent/BenchmarkTable.svelte";
-export { default as Table2 } from "./BenchmarkComponent/BenchmarkTable2.svelte";
