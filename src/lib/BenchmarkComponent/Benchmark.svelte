87c3339a06b9557aa68c3e48342cb5366e6335c1
Pau Figueras <pau@pswsm.cat>
2023-03-20 12:57:11 +0100

    Comment all props

--- src/lib/BenchmarkComponent/Benchmark.svelte
+++ src/lib/BenchmarkComponent/Benchmark.svelte
@@ -2,23 +2,50 @@
 import { Footer, FooterBrand, Card, FooterCopyright } from "flowbite-svelte";
 import { Banner, Navbar, Table, Contact, SocialButtons } from "$lib";
 
-// Component props
-export let pageTitle: string;
-export let pageDescription: string;
-export let apiUrls: {headerEndpoint: string, dataEndpoint: string};
-export let navObjs: {text: string, link: string}[];
-export let tableTitle: string = 'Leaderboard';
-export let tableSubtitle: string | null = null;
-export let footerProps: { images?: Array<{alt: string, src: string, href?: string, target?: string}>, texts?: string[] } = {};
-export let navColorScheme: { textColor?: "black" | "white", backgroundColor: "red" | "white" | "gray" | "white" | "blue", backgroundHardness?: 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 } = { textColor: "white", backgroundColor: "red", backgroundHardness: 700 };
-export let contact: string;
-export let backgroundColor: "white" | "black" | "neutral";
-export let spinnerColor: "red" | "white" | "gray" | "white" | "blue" = 'red';
+// let innerHeigth;
 
-// let modelsData: Promise<any> = fetch(apiUrl);
-// let tableHeaders: Promise<{tasks: object[]}>;
-// let tableHeaders: Promise<{ tasks: { _id: { "$oid": string }[], description: string, name: string }[] }> = fetch(apiUrls.headerEndpoint).then( (res) => res.json() ).catch( (err) => err );
-// let tableContent: Promise<{ results: {model: string, score: number, submitted_by: string, URL: string, task1: number, task2: number, task3: number, task4: number}[] }> = fetch(apiUrls.dataEndpoint).then((res) => res.json());
+//! Component props
+/// The page title
+export let pageTitle: string;
+
+/// The page description
+export let pageDescription: string;
+
+/// The api urls to pass to the table component
+export let apiUrls: {headerEndpoint: string, dataEndpoint: string};
+
+/// A list with the navbar elements
+export let navObjs: {text: string, link: string}[];
+
+/// The table title, which defaults to 'Leaderboard'
+export let tableTitle: string = 'Leaderboard';
+
+/// The table subtitile (optional)
+export let tableSubtitle: string | null = null;
+
+/// The images or texts of the footer
+export let footerProps: { images?: Array<{alt: string, src: string, href?: string, target?: string}>, texts?: string[] } | null = null;
+
+/// The navbar colorscheme
+export let navColorScheme: { textColor?: "black" | "white", backgroundColor: "red" | "white" | "gray" | "white" | "blue", backgroundHardness?: 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 } =
+	{
+		textColor: "white",
+		backgroundColor: "red",
+		backgroundHardness: 700
+	};
+
+/// The contact mail
+export let contact: string;
+
+/// The background color
+export let backgroundColor: "white" | "black" | "neutral";
+
+/// The color of the spinner component. Defaults to red
+export const spinnerColor: "red" | "white" | "gray" | "white" | "blue" = 'red';
+
+/// The name of the repo (for the star in github button on SocialButtons component)
+export let repoName: string;
+
 </script>
 <header>
 <Navbar {...navColorScheme} navObjs={navObjs} pageTitle={pageTitle} />
