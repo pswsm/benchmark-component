15c68365966d115033ec7fe2e0be8bfe4cb036c5
Pau Figueras <pau@pswsm.cat>
2023-03-20 12:50:23 +0100

    Change min-height to a style propert instead of class

--- src/lib/BenchmarkComponent/Banner.svelte
+++ src/lib/BenchmarkComponent/Banner.svelte
@@ -5,7 +5,7 @@
 	export let minHeight: string = "300px"
 </script>
 {#if text}
-	<div class="min-h-[{minHeight}] text-center flex justify-center items-center" style:background-color='{bgcolor}'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
+	<div class="text-center flex justify-center items-center" style:background-color='{bgcolor}' style:min-height='{minHeight}'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
 {:else}
-	<div class="min-h-[{minHeight}]" style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0), {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
+	<div style:min-height='{minHeight}' style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0), {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
 {/if}
