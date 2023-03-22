5de466d293989a26375f92eb65ebfe09886cb1e8
Pau Figueras <pau@pswsm.cat>
2023-03-22 12:44:15 +0100

    single dataset component

---
+++ src/lib/DatasetComponent/Dataset.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+export let datasetTitle: string;
+export let datasetBody: string;
+</script>
+<hr>
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ datasetTitle }</h5>
+<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{ datasetBody }</p>
