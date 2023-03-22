b0217d070b89f52c782a1773e8086e90aaebd01f
Pau Figueras <pau@pswsm.cat>
2023-03-22 12:46:35 +0100

    Actaully show table subtitle

--- src/lib/BenchmarkComponent/Benchmark.svelte
+++ src/lib/BenchmarkComponent/Benchmark.svelte
@@ -55,7 +55,7 @@ export let repoName: string;
 	<Card id="table-card" class="overflow-x-auto mx-auto rounded-none bg-black border-none my-4" size="xl" color="none">
 		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ tableTitle }</h5>
 		{#if tableSubtitle}
-			<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight"></p>
+			<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{ tableSubtitle }</p>
 		{/if}
 		<Table headerUrl={apiUrls.headerEndpoint} contentUrl={apiUrls.dataEndpoint} />
 	</Card>
