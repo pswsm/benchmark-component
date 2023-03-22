0ff004d0316c068110e544abe3cb6b7b9bb3154c
Pau Figueras <pau@pswsm.cat>
2023-03-22 12:43:42 +0100

    Export dataset0realted components

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,3 +6,5 @@ export { default as Table } from "./BenchmarkComponent/BenchmarkTable.svelte";
 export { default as Contact } from "./BenchmarkComponent/Contact.svelte";
 export { default as SocialButtons } from "./BenchmarkComponent/SocialButtons.svelte";
 export { default as BenchmarkFooter } from "./BenchmarkComponent/BenchmarkFooter.svelte";
+export { default as Datasets } from "./DatasetComponent/DatasetList.svelte";
+export { default as Dataset } from "./DatasetComponent/Dataset.svelte";
