1075b9937f9e36ab12d66c9d86fa78aae559d1af
Pau Figueras <pau@pswsm.cat>
2023-03-22 11:34:37 +0100

    Calculate min height from window width

--- src/lib/BenchmarkComponent/Banner.svelte
+++ src/lib/BenchmarkComponent/Banner.svelte
@@ -2,10 +2,15 @@
 	export let imgSrc: string;
 	export let bgcolor: string;
 	export let text: string | null = null;
-	export let minHeight: string = "300px"
+
+	let xWidth: number;
+
+	$: divHeight = (xWidth > 750) ? xWidth * 0.15 : xWidth * 0.20;
 </script>
+<svelte:window bind:outerWidth={xWidth}/>
+
 {#if text}
-	<div class="text-center flex justify-center items-center" style:background-color='{bgcolor}' style:min-height='{minHeight}'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
+	<div class="text-center flex justify-center items-center" style:background-color='{bgcolor}' style:min-height='{divHeight}px'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
 {:else}
-	<div style:min-height='{minHeight}' style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0), {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
+	<div style:min-height='{divHeight}px' style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0) 50%, {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
 {/if}
