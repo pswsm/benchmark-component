cb6abe040cf3746272d5ec2565aad8da76197d75
Pau Figueras <pau@pswsm.cat>
2023-03-28 13:35:38 +0200

    Add "tweet" button.

--- src/lib/BenchmarkComponent/SocialButtons.svelte
+++ src/lib/BenchmarkComponent/SocialButtons.svelte
@@ -3,7 +3,18 @@ export let repoName: string; // pswsm/some-repo
 </script>
 <svelte:head>
 	<!-- Place this tag in your head or just before your close body tag. -->
-	<script async defer src="https://buttons.github.io/buttons.js"></script>
+	<script src="https://buttons.github.io/buttons.js"></script>
 </svelte:head>
+<div class="felx items-center justify left">
 <!-- Place this tag where you want the button to render. -->
 <a class="github-button" href="https://github.com/{repoName}" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" aria-label="Star {repoName} on GitHub">Star</a>
+<span><a href="https://twitter.com/share" class="twitter-share-button" data-url="https://www.projecteaina.cat/" data-text="Catalan Language Understanding Benchmark" data-via="projecte_aina" data-size="large" data-hashtags="CLUB" target="_blank" rel="noreferrer">Tweet</a></span>
+</div>
+<style>
+a {
+	color: #ffce2d;
+}
+a:hover {
+	color: red;
+}
+</style>
