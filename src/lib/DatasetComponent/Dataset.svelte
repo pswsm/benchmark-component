94c07eab35322b3350f0b6e7e990386cd45b6c12
Pau Figueras <pau@pswsm.cat>
2023-03-28 13:11:25 +0200

    Better format

--- src/lib/DatasetComponent/Dataset.svelte
+++ src/lib/DatasetComponent/Dataset.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
-export let datasetTitle: string;
-export let datasetBody: string;
+export let title: string;
+export let description: string;
+export let link: string;
 </script>
-<hr>
-<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ datasetTitle }</h5>
-<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{ datasetBody }</p>
+<div class="my-6">
+	<hr>
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-white text-white"><a href="{link}">{ title }</a></h5>
+	<p class="font-normal text-white leading-tight">{ description }</p>
+</div>
+<style>
+a {
+	color: #ffce2d;
+}
+a:hover {
+	color: red;
+}
+</style>
