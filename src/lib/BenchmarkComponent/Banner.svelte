bd9e78229b795c17e35b27ea7083cc7daecd6bb2
Pau Figueras <pau@pswsm.cat>
2023-03-28 13:10:34 +0200

    Prioritize images

--- src/lib/BenchmarkComponent/Banner.svelte
+++ src/lib/BenchmarkComponent/Banner.svelte
@@ -9,8 +9,8 @@
 </script>
 <svelte:window bind:outerWidth={xWidth}/>
 
-{#if text}
-	<div class="text-center flex justify-center items-center" style:background-color='{bgcolor}' style:min-height='{divHeight}px'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
-{:else}
+{#if imgSrc}
 	<div style:min-height='{divHeight}px' style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0) 50%, {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
+{:else}
+	<div class="text-center flex justify-center items-center" style:background-color='{bgcolor}' style:min-height='{divHeight}px'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
 {/if}
