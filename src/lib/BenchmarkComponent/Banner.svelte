cf158a88e12f096a4e83d86c7273d791b0cdb3c7
Pau Figueras <pau@pswsm.cat>
2023-03-28 13:35:22 +0200

    Add text color prop

--- src/lib/BenchmarkComponent/Banner.svelte
+++ src/lib/BenchmarkComponent/Banner.svelte
@@ -2,6 +2,8 @@
 	export let imgSrc: string;
 	export let bgcolor: string;
 	export let text: string | null = null;
+	export let subtitle: string | null = null;
+	export let color: 'white' | 'black' = 'black';
 
 	let xWidth: number;
 
@@ -12,5 +14,8 @@
 {#if imgSrc}
 	<div style:min-height='{divHeight}px' style:background-image='linear-gradient(to bottom, rgba(34, 34, 34, 0) 50%, {bgcolor}), url("{imgSrc}")' style:background-repeat="no-repeat" style:background-size="auto, cover"></div>
 {:else}
-	<div class="text-center flex justify-center items-center" style:background-color='{bgcolor}' style:min-height='{divHeight}px'><h1 class='text-[7vw] capitalize'>{ text }</h1></div>
+	<div class="text-center py-6" style:background-color='{bgcolor}' style:color='{color}'>
+		<h1 class='text-[5vw] capitalize align-middle inline'>{ text }</h1>
+		<p class="sub">{ subtitle }</p>
+	</div>
 {/if}
