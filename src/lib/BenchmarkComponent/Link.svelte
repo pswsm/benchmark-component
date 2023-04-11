26a534a8952ac39c2af7660a25d45ad389034560
alexcumplido <acumpbo@gmail.com>
2023-04-11 10:02:37 +0200

    Created hover effect with mouse events

---
+++ src/lib/BenchmarkComponent/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { styles } from "$lib/styles.js";
+    import { linkColor, hoverColor } from "$lib/stores.js";
+    export let contact: string;
+    let colour = $linkColor;
+    const handleMouseOver = () => colour = `${$linkColor}`;
+    const handleMouseOut = () => colour = `${$hoverColor}`;
+
+</script>
+    <a style:color={colour} on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} use:styles={{ colour}} href="mailto:{contact}">{contact}</a>
+<style>
+
+</style>
