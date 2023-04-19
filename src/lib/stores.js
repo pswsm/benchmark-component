81868b439d53686d1da6106c54a7e4a6dabebf9d
Pau Figueras <pau@pswsm.cat>
2023-04-19 13:05:38 +0200

    [WIP] Nav colorscheme store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,4 +3,7 @@ import { readable, writable } from "svelte/store";
 export let linkColor = readable("#ffce2d");
 export let hoverColor = readable("red");
 
+export let navColorSchemeLoad = writable();
+export let navColorScheme = readable(navColorSchemeLoad);
+
 export let benchmarkConfig = writable({});
